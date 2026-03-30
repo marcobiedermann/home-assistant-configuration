567dc8f09a129ad48fa0747339bbcec805355beb
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2026-03-30 19:05:53 +0200

    Auto backup 2026-03-30 19:05:53

--- git_backup.sh
+++ git_backup.sh
@@ -2,10 +2,15 @@
 
 cd /config || exit 1
 
+echo "[$(date)] Starting backup"
+
 git pull --rebase
 
 if ! git diff --quiet; then
   git add .
   git commit -m "Auto backup $(date '+%Y-%m-%d %H:%M:%S')"
   git push
+  echo "[$(date)] Changes pushed"
+else
+  echo "[$(date)] No changes"
 fi
